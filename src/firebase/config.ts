14998eee5eb13ef48e8ebbebd5c570f0e891b83c
Harshith Naidu B 2024028790 <hbandi@student.gitam.edu>
2026-01-03 17:25:01 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-93088908-94bd7",
+  "appId": "1:306585006388:web:96f04a50a1a5df2836b561",
+  "apiKey": "AIzaSyAxmQ9s2FM9a29EVQ3VkE287VPSoXxqpEs",
+  "authDomain": "studio-93088908-94bd7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "306585006388"
+};
